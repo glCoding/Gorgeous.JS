36206be81914bf7c2a7bcdefbe0baa6774b56277
foolyou <foolyou.chen@outlook.com>
2015-05-22 18:31:47 +0800

    n

--- test/js/Arithmetic Operations.js
+++ test/js/Arithmetic Operations.js
@@ -1,21 +1,18 @@
 //Test:Arithmetic Operations Test
 var g = gorgeous;
-var src = 'img/baboon.png';
 var canvas1 = document.createElement('canvas');
 var ctx1 = canvas1.getContext('2d');
 var canvas2 = document.createElement('canvas');
 var ctx2 = canvas2.getContext('2d');
-var img = g.loadImage(src, function (img) {
-	canvas1.width = 120;
-	canvas1.height = 120;
-	ctx1.fillStyle = 'rgb(225, 10, 101)';
-	ctx1.fillRect(0, 0, 120, 120);
-	canvas2.width = 120;
-	canvas2.height = 120;
-	ctx2.fillStyle = 'rgb(0, 21, 190)';
-	ctx2.fillRect(0, 0, 120, 120);
-	tests();
-});
+canvas1.width = 120;
+canvas1.height = 120;
+ctx1.fillStyle = 'rgb(225, 10, 101)';
+ctx1.fillRect(0, 0, 120, 120);
+canvas2.width = 120;
+canvas2.height = 120;
+ctx2.fillStyle = 'rgb(0, 21, 190)';
+ctx2.fillRect(0, 0, 120, 120);
+tests();
 
 function tests() {
 Test(
